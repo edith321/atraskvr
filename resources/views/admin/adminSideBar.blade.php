cb2cdbd24deae88e8386ead19ca750dcbe58362f
Edita <edita.paulikaite@inbox.lt>
2017-06-14 12:26:08 +0300

    fixed sidebar routes

--- resources/views/admin/adminSideBar.blade.php
+++ resources/views/admin/adminSideBar.blade.php
@@ -4,11 +4,11 @@
         <a href="{{ url('/admin/pages/')}}">{{trans('app.adminMenuPages')}}</a><br/>
         <a href="{{ url('/admin/categories/')}}">{{trans('app.adminMenuCategories')}}</a><br/>
         <a href="{{ url('/admin/---/')}}">{{trans('app.adminMenuResources')}}</a><br/>
-        <a href="{{ url('/admin/---/')}}">{{trans('app.adminMenuOrders')}}</a><br/>
+        <a href="{{ url('/admin/orders/')}}">{{trans('app.adminMenuOrders')}}</a><br/>
 
         <a href="{{ url('/admin/languages/')}}">{{trans('app.adminMenuLanguages')}}</a><br/>
 
         <a href="{{ url('/admin/users/')}}">{{trans('app.adminMenuUsers')}}</a><br/>
-        <a href="{{ url('/admin/menus/')}}">{{trans('app.adminMenuMenus')}}</a><br/>
+        <a href="{{ url('/admin/menu/')}}">{{trans('app.adminMenuMenus')}}</a><br/>
     </div>
 </div>
